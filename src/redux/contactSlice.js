3757247fc48cdb2d4a6cad7f3a5af34370867bb4
VadimKorobec <_s_e_t__@ukr.net>
2023-02-25 13:27:04 +0200

    Create contactSlice.js

---
+++ src/redux/contactSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { INITIAL_CONTACTS } from './constants';
+
+export const contactSlice = createSlice({
+  name: 'contacts',
+  initialState: INITIAL_CONTACTS,
+  reducers: {
+    addContact(state, action) {
+      state.push(action.payload);
+    },
+    deleteContact(state, action) {
+      return state.filter(contact => contact.id !== action.payload);
+    },
+  },
+});
+
+export const contactsReducer = contactSlice.reducer;
+
+export const { addContact, deleteContact } = contactSlice.actions;

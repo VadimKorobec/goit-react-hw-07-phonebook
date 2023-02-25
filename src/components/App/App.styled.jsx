38da45a3145597adea5a8b2b7a2c8420ce5e8b9d
VadimKorobec <_s_e_t__@ukr.net>
2023-02-25 12:57:15 +0200

    Create App.styled.jsx

---
+++ src/components/App/App.styled.jsx
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  width: 400px;
+  margin: 10px auto;
+  text-align: center;
+  padding: 10px 0;
+  border: 1px solid #cccaca;
+  border-radius: 5px;
+  background-color: #f7f7f7;
+  box-shadow: 0 5px 15px rgb(0 0 0 / 35%);
+`;

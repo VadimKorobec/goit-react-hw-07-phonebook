215446fcb5ce260b6621b10046dee1ba987e8f0a
VadimKorobec <_s_e_t__@ukr.net>
2023-02-25 12:57:03 +0200

    Create ContactsListItem.styled.jsx

---
+++ src/components/ContactsListItem/ContactsListItem.styled.jsx
@@ -0,0 +1,19 @@
+import styled from '@emotion/styled';
+
+export const Item = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const Button = styled.button`
+  margin-left: 40px;
+  cursor: pointer;
+  padding: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+`;
+
+export const Text = styled.span`
+  margin-right: auto;
+`;

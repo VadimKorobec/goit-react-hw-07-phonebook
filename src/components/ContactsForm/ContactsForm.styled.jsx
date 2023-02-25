12917eb20fb24241c1d0d0cb1284306dca035fe5
VadimKorobec <_s_e_t__@ukr.net>
2023-02-25 12:57:11 +0200

    Create ContactsForm.styled.jsx

---
+++ src/components/ContactsForm/ContactsForm.styled.jsx
@@ -0,0 +1,22 @@
+import styled from '@emotion/styled';
+
+export const Form = styled.form``;
+
+export const Label = styled.label`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Input = styled.input`
+  width: 200px;
+  margin: 0 auto;
+  margin-bottom: 15px;
+  border-radius: 5px;
+  height: 25px;
+`;
+
+export const Button = styled.button`
+  padding: 10px;
+  border-radius: 5px;
+  cursor: pointer;
+`;

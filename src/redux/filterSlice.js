16bf60fdb79b1e5173582ceb7cb6f2615be0e551
VadimKorobec <_s_e_t__@ukr.net>
2023-02-26 16:35:47 +0200

    Update filterSlice.js

--- src/redux/filterSlice.js
+++ src/redux/filterSlice.js
@@ -1,16 +1,18 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-export const filterSlice = createSlice({
-  name: 'filter',
-  initialState: '',
+const filtersInitialState = '';
+
+const filterSlice = createSlice({
+  name: 'filtres',
+  initialState: {
+    search: filtersInitialState,
+  },
   reducers: {
-    setFilter(state, action) {
-      state = action.payload;
-      return state;
+    searchContact: (state, action) => {
+      state.search = action.payload;
     },
   },
 });
 
-export const filterReducer = filterSlice.reducer;
-
-export const { setFilter } = filterSlice.actions;
+export const { searchContact } = filterSlice.actions;
+export const filtersReducer = filterSlice.reducer;

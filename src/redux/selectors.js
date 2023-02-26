28d8f81c97b08d4b16eddf2c710d171fa773a882
VadimKorobec <_s_e_t__@ukr.net>
2023-02-26 15:52:53 +0200

    Update selectors.js

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,5 +1,3 @@
-import { createSelector } from '@reduxjs/toolkit';
-
 export const getContacts = state => state.contacts.items;
 
 export const getIsLoading = state => state.contacts.IsLoading;
@@ -7,13 +5,3 @@ export const getIsLoading = state => state.contacts.IsLoading;
 export const getError = state => state.contacts.error;
 
 export const getFilter = state => state.filter;
-
-export const getFilteredConctacts = createSelector(
-  [getContacts, getFilter],
-  (contacts, filter) => {
-    const normalizedFilter = filter;
-    return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(normalizedFilter)
-    );
-  }
-);

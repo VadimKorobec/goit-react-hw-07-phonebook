c9f4745d5c750099971b1f78e72e53a033e5a2af
VadimKorobec <_s_e_t__@ukr.net>
2023-02-26 13:05:08 +0200

    Update operations.js

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -3,7 +3,14 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 
 axios.defaults.baseURL = 'https://63f6810f59c944921f75449f.mockapi.io';
 
-export const fetchContacts = createAsyncThunk('contacts/fetchAll', async () => {
-  const response = await axios.get('/contacts');
-  return response.data;
-});
+export const fetchContacts = createAsyncThunk(
+  'contacts/fetchAll',
+  async (_, thunkAPI) => {
+    try {
+      const response = await axios.get('/contacts');
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);

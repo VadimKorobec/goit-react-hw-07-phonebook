2672d95981bd743e532451ee3444d6ccb6d78273
VadimKorobec <_s_e_t__@ukr.net>
2023-02-26 15:24:46 +0200

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,16 @@
-// import { configureStore, combineReducers } from '@reduxjs/toolkit';
-// import storage from 'redux-persist/lib/storage';
-// import {
-//   persistStore,
-//   persistReducer,
-//   FLUSH,
-//   REHYDRATE,
-//   PAUSE,
-//   PERSIST,
-//   PURGE,
-//   REGISTER,
-// } from 'redux-persist';
-// import { contactsReducer } from './contactSlice';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import { contactsReducer } from './contactSlice';
 // import { filterReducer } from './filterSlice';
 
 // const persistConfig = {
@@ -19,32 +19,32 @@
 //   whitelist: ['contacts'],
 // };
 
-// const rootReducer = combineReducers({
-//   contacts: contactsReducer,
-//   filter: filterReducer,
-// });
+const rootReducer = combineReducers({
+  contacts: contactsReducer,
+  //  filter: filterReducer,
+});
 
 // const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-// export const store = configureStore({
-//   reducer: persistedReducer,
-//   middleware: getDefaultMiddleware =>
-//     getDefaultMiddleware({
-//       serializableCheck: {
-//         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-//       },
-//     }),
-// });
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
 
 // export const persistor = persistStore(store);
 
-import { configureStore } from '@reduxjs/toolkit';
-import { contactsReducer } from './contactSlice';
-import { filterReducer } from './filterSlice';
+// import { configureStore } from '@reduxjs/toolkit';
+// import { contactsReducer } from './contactSlice';
+// import { filterReducer } from './filterSlice';
 
-export const store = configureStore({
-  reducer: {
-    contacts: contactsReducer,
-    filters: filterReducer,
-  },
-});
+// export const store = configureStore({
+//   reducer: {
+//     contacts: contactsReducer,
+//     filters: filterReducer,
+//   },
+// });
